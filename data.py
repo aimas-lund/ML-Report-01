2b228a267da97e10132edbbea5fef77cbe5d91ba
Aimas Lund <aimas.lund@gmail.com>
2020-02-22 15:01:29 +0100

    Inserted formatting function

--- data.py
+++ data.py
@@ -1,8 +1,34 @@
 from auxiliary import one_out_of_k
 import numpy as np
 
-class Data():
-    def __init__(self):
-        self.path = "./res/marketing.data"
+def load(path):
+    file = open(path, 'r')
+    path = file.readlines()
+    file.close()
 
-    def
+    return path
+
+def format(data):
+
+    remove_index = []
+
+    for i in range(len(data)):
+        if "NA" in data[i]:
+            remove_index.append(i)
+
+    for index in sorted(remove_index, reverse=True):
+        del data[index]
+
+    for j in range(len(data)):
+        data[j] = data[j].replace('\n','').split(' ')
+
+        for k in range(len(data[j])):
+            data[j][k] = int(data[j][k])
+
+    data = np.array(data)
+    print(data.shape)
+    return data
+
+path = "./res/marketing.data"
+data = load(path)
+format(data)
