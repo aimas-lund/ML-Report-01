cdcdbb22f7062024a139d627946409089684569f
Aimas Lund <aimas.lund@gmail.com>
2020-02-22 19:47:16 +0100

    Modified one-out-of-K function to accept matrices instead of arrays

--- auxiliary.py
+++ auxiliary.py
@@ -8,6 +8,8 @@ import numpy as np
 import pandas as pd
 
 """"""
+
+
 def load_csv(path):
     """
     Reads a file (csv) and formats it as specified.
@@ -20,43 +22,43 @@ def load_csv(path):
 
     # formats gender to numerical, "boolean" value
     for i in range(len(data)):
-        if data[i,3] == 'mal':
-            data[i,3] = 0
-        elif data[i,3] == 'fem':
-            data[i,3] = 1
+        if data[i, 3] == 'mal':
+            data[i, 3] = 0
+        elif data[i, 3] == 'fem':
+            data[i, 3] = 1
         else:
-            data[i,3] = -1
+            data[i, 3] = -1
 
     return headers, data
 
 
-def one_out_of_k(input, return_uniques=False):
+def one_out_of_k(input, column_index=0, return_uniques=False):
     """
-    Function returning the One-out-of-K of a given array.
-    :param input, return_uniques: 1xN Numpy array, boolean specifying if the unique values of the input should be returned
+    Function replacing a specified column of matrix with the One-out-of-K equivalent.
+    :param input:
+    :param column_index:
+    :param return_uniques: 1xN Numpy array, boolean specifying if the unique values of the input should be returned
     :return: The One-out-of-K numpy-matrix and optionally the unique values from the input array.
     """
-    uniques = np.unique(input)
+    chosen_column = input[:, column_index]
+    uniques = np.unique(chosen_column)
 
     # creates a matrix of 0's with number of unique entries as columns 
     # and number of input rows as row numbers
-    output = np.array(np.zeros(len(uniques) * len(input))).reshape(len(input), len(uniques))
+    output = np.array(np.zeros(len(uniques) * len(chosen_column))).reshape(len(chosen_column), len(uniques))
 
     # create a dictionary to lookup the corresponding index of the values in input
     lookup = dict(zip(uniques, np.arange(len(uniques))))
 
     # insert 1 in correct columns, iterating through the rows
-    for i in range(len(input)):
-        j = lookup[input[i]]
+    for i in range(len(chosen_column)):
+        j = lookup[chosen_column[i]]
         output[i, j] = 1
+
+    output = np.hstack((np.hstack((input[:, :column_index], output)), input[:, column_index + 1:]))
 
     # output uniques and output binary matrix in tuple
     if return_uniques:
         return uniques, output
     else:
         return output
-
-path = "./res/bone_density.csv"
-headers, data = load_csv(path)
-print(headers)
-print(data)
