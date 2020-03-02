73d90f3dfdec7cc62d74d902cb8d809196ce12d5
SkovenForrest <40027872+SkovenForrest@users.noreply.github.com>
2020-03-02 09:59:11 +0100

    Correlation
    Correlation

--- summary.py
+++ summary.py
@@ -1,5 +1,6 @@
 from auxiliary import one_out_of_k, add_elements_to_list
 import matplotlib.pyplot as plt
+from scipy import stats
 from scipy.linalg import svd
 import numpy as np
 import seaborn as sb
@@ -30,6 +31,7 @@ data_max = data.max(axis=0)
 
 box_data = df_data.drop(['mode', 'key', 'loudness', 'tempo', 'time_signature', 'popularity_interval', 'duration_ms'],
                         axis=1)
+"""
 f, ax = plt.subplots()
 ax = sb.boxplot(data=box_data, showfliers=False)
 plt.xticks(rotation=-15)
@@ -52,11 +54,11 @@ sb.boxplot(data=df_data['loudness'], showfliers=False, color='purple', orient='h
 plt.xlabel("Loudness (dB)")
 plt.suptitle('Categorical Plotting')
 plt.show()
-
+"""
 ###############################################
 # Seaborn Plotting
 ###############################################
-
+"""
 palette = ['blue', 'purple', 'red', 'green', 'black']
 # print plot of energy vs loudness
 sb.set(style="ticks", rc={'figure.figsize': (16, 6)})
@@ -66,7 +68,7 @@ sb.relplot(x="energy", y="loudness",
            col="popularity_interval",
            alpha=0.5,
            data=df_data)
-
+"""
 ###############################################
 # Principal Component Analysis
 ###############################################
@@ -79,6 +81,140 @@ U, S, V = svd(Y, full_matrices=False)
 # Compute variance explained by principal components
 rho = (S * S) / (S * S).sum()
 
+# scipy.linalg.svd returns "Vh", which is the Hermitian (transpose)
+# of the vector V. So, for us to obtain the correct V, we transpose:
+V = V.T
+
+# Project the centered data onto principal component space
+Z = Y @ V
+# Indices of the principal components to be plotted
+"""
+i = 3
+j = 7
+
+# Plot PCA of the data
+f = plt.figure()
+plt.title('PCA')
+color = np.array(['b','m','r','g','k'])
+#Z = array(Z)
+for c in range(C):
+    # select indices belonging to class c:
+    class_mask = y==c+1
+    plt.plot(Z[class_mask,i], Z[class_mask,j], 'o', 
+             alpha=.5, 
+             c=color[class_mask])
+plt.legend(class_names)
+plt.xlabel('PC{0}'.format(i+1))
+plt.ylabel('PC{0}'.format(j+1))
+
+# Output result to screen
+plt.show()
+
+i = 6
+j = 8
+
+# Plot PCA of the data
+f = plt.figure()
+plt.title('PCA')
+#Z = array(Z)
+for c in range(C):
+    # select indices belonging to class c:
+    class_mask = y==c+1
+    plt.plot(Z[class_mask,i], Z[class_mask,j], 'o', 
+             alpha=.5, 
+             c=np.array(['w','b','m','r','g','k']))
+plt.legend(class_names)
+plt.xlabel('PC{0}'.format(i+1))
+plt.ylabel('PC{0}'.format(j+1))
+
+# Output result to screen
+plt.show()
+"""
+
+i = 3
+j = 7
+f = plt.figure()
+plt.title('PCA')
+
+#Z = array(Z)
+for c in range(C):
+    # select indices belonging to class c:
+    class_mask = y==c+1
+    plt.plot(Z[class_mask,i], Z[class_mask,j], 'o', 
+             alpha=.5)
+plt.legend(class_names)
+plt.xlabel('PC{0}'.format(i+1))
+plt.ylabel('PC{0}'.format(j+1))
+
+# Output result to screen
+plt.show()
+i = 6
+j = 8
+
+# Plot PCA of the data
+f = plt.figure()
+plt.title('PCA')
+#Z = array(Z)
+for c in range(C):
+    # select indices belonging to class c:
+    class_mask = y==c+1
+    plt.plot(Z[class_mask,i], Z[class_mask,j], 'o', 
+             alpha=.5)
+plt.legend(class_names)
+plt.xlabel('PC{0}'.format(i+1))
+plt.ylabel('PC{0}'.format(j+1))
+
+# Output result to screen
+plt.show()
+
+from scipy import stats
+
+######################################
+#Histogram
+######################################
+"""
+# Number of bins in histogram
+nbins = 20
+
+# Plot the histogram
+f = plt.figure()
+plt.title('Normal distribution')
+plt.hist(X[:,0], bins=nbins, density=False)
+
+# Over the histogram, plot the theoretical probability distribution function:
+x = np.linspace(X[:,0].min(), X[:,0].max(), 1000)
+pdf = stats.norm.pdf(x,loc=17,scale=2)
+plt.plot(x,pdf,'.',color='red')
+"""
+#hist = sb.distplot(X[:,2],norm_hist=True)
+plt.hist(X[:, 15], bins=200)
+plt.show()
+
+
+df = pd.DataFrame(X)
+plt.matshow(df.corr())
+plt.colorbar()
+plt.show()
+
+"""
+for i in range(19):
+    for j in range(19):
+# Plot PCA of the data
+        f = plt.figure()
+        plt.title('PCA')
+        #Z = array(Z)
+        for c in range(C):
+            # select indices belonging to class c:
+            class_mask = y==c+1
+            plt.plot(Z[class_mask,i], Z[class_mask,j], 'o', alpha=.5)
+        plt.legend(class_names)
+        plt.xlabel('PC{0}'.format(i+1))
+        plt.ylabel('PC{0}'.format(j+1))
+        
+        # Output result to screen
+        plt.show()
+"""
+"""
 threshold = 0.9
 # Plot variance explained
 f2 = plt.figure()
@@ -93,17 +229,12 @@ plt.ylabel('Variance explained')
 plt.legend(['Individual', 'Cumulative', 'Threshold'])
 plt.grid()
 plt.show()
-
+"""
 ###############################################
 # Principal Component Analysis Algorithm
 ###############################################
 
-# scipy.linalg.svd returns "Vh", which is the Hermitian (transpose)
-# of the vector V. So, for us to obtain the correct V, we transpose:
-V = V.T
 
-# Project the centered data onto principal component space
-Z = Y @ V
 
 """
 # Indices of the principal components to be plotted
@@ -142,6 +273,7 @@ attribute_names = np.delete(attribute_names,
 attribute_names = np.delete(attribute_names,
                             np.where(attribute_names == 'popularity_interval'))
 """
+"""
 pca_names = []
 coeffs = []
 for i in range(len(rho)):
@@ -152,7 +284,7 @@ pca_df = pd.DataFrame(data=V, columns=pca_names)
 
 
 def trim_axs(axs, N):
-    """little helper to massage the axs list to have correct length..."""
+    """"""little helper to massage the axs list to have correct length...""""""
     axs = axs.flat
     for ax in axs[N:]:
         ax.remove()
@@ -209,3 +341,4 @@ axs_pca.axhline(linewidth=1, color='black')
 axs_pca.set_xticklabels(coeffs, rotation=45)
 
 plt.show()
+"""
