dff2bbcd6d7d51b4bae3e3f716bbc8c840df9433
Aimas Lund <aimas.lund@gmail.com>
2020-03-24 17:30:24 +0100

    Linear Regression model implemented

--- linear_model.py
+++ linear_model.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split, cross_val_predict,cross_val_score
 from sklearn import metrics
 import matplotlib.pyplot as plt
-
+from auxiliary import every_nth
 
 file_path = "./res/spotify-data-apr-2019.csv"
 df_data = pd.read_csv(file_path)            # data as pandas DataFrame format
@@ -13,11 +13,11 @@ attribute_names = df_data.columns.values    # numpy array of attribute names
 df_data.sample(frac=1)                      # shuffle rows of data
 data = df_data.values                       # all data in numpy array format
 
-vf = 0.2                    # fraction of the data reserved for validation (usually between 0.2 and 0.3)
-N = data.shape[0]           # number of rows in the dataset
-M = data.shape[1]           # number of columns in the dataset
-y = data[:, M-1:]             # class belonging to each row in normal format
-X = data[:, :M-1]             # data set with all non-class attributes
+vf = 0.2                                    # fraction of the data reserved for validation (usually between 0.2 and 0.3)
+N = data.shape[0]                           # number of rows in the dataset
+M = data.shape[1]                           # number of columns in the dataset
+y = data[:, 10]                             # class belonging to each row in normal format
+X = np.delete(data, 10, axis=1)
 
 # create training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=vf)
@@ -28,16 +28,15 @@ lin_model = lm.LinearRegression()
 
 # the test set we keep for testing
 # the training set we use k-folds cross validation
-folds = 10
+folds = 15
 scores = cross_val_score(lin_model, X_train, y_train, cv=folds)
 print(scores)
 
 predictions = cross_val_predict(lin_model, X_test, y_test, cv=folds)
-ticks = list(range(1, 6))
-plt.scatter(y_test, predictions)
-plt.xticks(ticks)
-plt.yticks(ticks)
+plt.scatter(y_test, predictions, marker='x')
+plt.xlabel("True Tempo")
+plt.ylabel("Predicted Tempo")
 plt.show()
 
 accuracy = metrics.r2_score(y_test, predictions)
-print(accuracy)
+print("Accuracy: {}".format(accuracy))
