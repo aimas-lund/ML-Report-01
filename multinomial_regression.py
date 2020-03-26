a1d6244f857098c8bd22aac7401912ec853c6d07
SkovenForrest <40027872+SkovenForrest@users.noreply.github.com>
2020-03-26 15:36:46 +0100

    Forsøg på at få ANN classifikation eller multinomial regression til at virke

--- multinomial_regression.py
+++ multinomial_regression.py
@@ -1,3 +1,28 @@
+# exercise 8.3.2 Fit multinomial regression
+from matplotlib.pyplot import figure, show, title
+from scipy.io import loadmat
+from toolbox_02450 import dbplotf, train_neural_net, visualize_decision_boundary
+import numpy as np
+import sklearn.linear_model as lm
+
+# Load Matlab data file and extract variables of interest
+
+"""
+mat_data = loadmat('../Data/synth1.mat')
+X = mat_data['X']
+X = X - np.ones((X.shape[0],1)) * np.mean(X,0)
+X_train = mat_data['X_train']
+X_test = mat_data['X_test']
+y = mat_data['y'].squeeze()
+y_train = mat_data['y_train'].squeeze()
+y_test = mat_data['y_test'].squeeze()
+
+attributeNames = [name[0] for name in mat_data['attributeNames'].squeeze()]
+classNames = [name[0][0] for name in mat_data['classNames']]
+
+N, M = X.shape
+C = len(classNames)
+"""
 from auxiliary import one_out_of_k, add_elements_to_list
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure, show, title
@@ -37,8 +62,7 @@ y = data[:,[13]]             # Tempo(target)
 selector = [x for x in range(data.shape[1]) if x != 13]
 X = data[:, selector] # the rest of the data set
 #X = data[:,:14]           # the rest of features
-N, M = X.shape
-C = 2
+
 attributeNames = ['acousticness', 'danceability', 'duration_ms', 'energy',
                   'instrumentalness', 'key','liveness', 'loudness', 'mode',
                    'speechiness', 'time_signature', 'valence', 'popularity_interval']
@@ -65,43 +89,21 @@ N, M = X.shape
 C = len(classNames)
 #%% Model fitting and prediction
 
-# Define the model structure
-n_hidden_units = 5 # number of hidden units in the signle hidden layer
-model = lambda: torch.nn.Sequential(
-                            torch.nn.Linear(M, n_hidden_units), #M features to H hiden units
-                            torch.nn.ReLU(), # 1st transfer function
-                            # Output layer:
-                            # H hidden units to C classes
-                            # the nodes and their activation before the transfer 
-                            # function is often referred to as logits/logit output
-                            torch.nn.Linear(n_hidden_units, C), # C logits
-                            # To obtain normalised "probabilities" of each class
-                            # we use the softmax-funtion along the "class" dimension
-                            # (i.e. not the dimension describing observations)
-                            torch.nn.Softmax(dim=1) # final tranfer function, normalisation of logit output
-                            )
-# Since we're training a multiclass problem, we cannot use binary cross entropy,
-# but instead use the general cross entropy loss:
-loss_fn = torch.nn.CrossEntropyLoss()
-# Train the network:
-net, _, _ = train_neural_net(model, loss_fn,
-                             X=torch.tensor(X_train, dtype=torch.float),
-                             y=torch.tensor(y_train, dtype=torch.long),
-                             n_replicates=3,
-                             max_iter=10000)
-# Determine probability of each class using trained network
-softmax_logits = net(torch.tensor(X_test, dtype=torch.float))
-# Get the estimated class as the class with highest probability (argmax on softmax_logits)
-y_test_est = (torch.max(softmax_logits, dim=1)[1]).data.numpy() 
-# Determine errors
-e = (y_test_est != y_test)
-print('Number of miss-classifications for ANN:\n\t {0} out of {1}'.format(sum(e),len(e)))
+# Multinomial logistic regression
+logreg = lm.LogisticRegression(solver='lbfgs', multi_class='multinomial', tol=1e-4, random_state=1)
+logreg.fit(X_train,y_train)
 
-predict = lambda x:  (torch.max(net(torch.tensor(x, dtype=torch.float)), dim=1)[1]).data.numpy() 
-figure(1,figsize=(9,9))
+# To display coefficients use print(logreg.coef_). For a 4 class problem with a 
+# feature space, these weights will have shape (4, 2).
+
+# Number of miss-classifications
+print('Number of miss-classifications for Multinormal regression:\n\t {0} out of {1}'.format(np.sum(logreg.predict(X_test)!=y_test),len(y_test)))
+
+predict = lambda x: np.argmax(logreg.predict_proba(x),1)
+figure(2,figsize=(9,9))
 visualize_decision_boundary(predict, [X_train, X_test], [y_train, y_test], attributeNames, classNames)
-title('ANN decision boundaries')
+title('LogReg decision boundaries')
 
 show()
 
-print('Ran Exercise 8.3.1')
+print('Ran Exercise 8.3.2')
